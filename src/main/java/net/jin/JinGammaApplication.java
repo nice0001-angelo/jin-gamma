6cdcb56fc41a2d3d0805da249c9e1151eeb2189e
nice0001-angelo <nice0001@gmail.com>
2020-02-10 22:39:52 -0500

    jin-gamma initial files

---
+++ src/main/java/net/jin/JinGammaApplication.java
@@ -0,0 +1,20 @@
+/*
+ * This is a Jin-gamma Project
+ * File name : JinGammaApplication.java
+ * Created by : Jinhyun
+ * Created on : Jan 2020
+ * Contents : Main Application.java
+ */
+package net.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinGammaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinGammaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/net/jin/controller/UsersController.java
@@ -0,0 +1,36 @@
+/*
+ * This is a Jin-gamma Project
+ * File name : UsersController.java
+ * Created by : Jinhyun
+ * Created on : Jan 2020
+ * Contents : for the UsersController
+ */
+package net.jin.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import net.jin.repository.UsersRepository;
+import net.jin.service.JoinService;
+
+@Controller
+public class UsersController {
+
+	@Autowired
+	private UsersRepository usersRepository;
+	
+	@PostMapping(value="/joinRequest")
+	public String joinRequest(HttpServletRequest request) {
+		JoinService joinService = new JoinService();
+		joinService.joinUser(request, usersRepository);
+		return "index";
+	}
+	
+	@PostMapping(value="/loginRequest")
+	public String loginRequest() {
+		return "index";
+	}
+}
