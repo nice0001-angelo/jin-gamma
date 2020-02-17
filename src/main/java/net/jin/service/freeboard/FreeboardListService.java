568d04a32ba8ee675433a36bb6fe0b9ae2b323dc
nice0001-angelo <nice0001@gmail.com>
2020-02-17 03:55:55 -0500

    update freeboard

--- src/main/java/net/jin/service/freeboard/FreeboardListService.java
+++ src/main/java/net/jin/service/freeboard/FreeboardListService.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import net.jin.model.Freeboard;
+import net.jin.pageMaker.PageMaker;
 import net.jin.repository.FreeboardRepository;
 
 @Service
@@ -28,15 +29,20 @@ public class FreeboardListService {
 
 	public String freeboardList(int pageNum){
 		
+		PageMaker pageMaker = pageMakerService.generatePageMaker(pageNum, 10, freeboardRepository);
+		
 		PageRequest pageRequest = PageRequest.of(pageNum-1, 10, Sort.Direction.DESC , "freeboardid");
 		Page<Freeboard> freeboardPage = freeboardRepository.findAll(pageRequest);
 		
 		if(freeboardPage.getSize() == 0) {
 			session.setAttribute("boardList", new ArrayList<Freeboard>());
+			session.setAttribute("pageMaker", pageMaker);
 			return "freeboard";
 		}
 		List<Freeboard> freeboardList = freeboardPage.getContent();
 		session.setAttribute("boardList", freeboardList);
+		session.setAttribute("pageMaker", pageMaker);
+		
 		return "freeboard";
 	}
 
