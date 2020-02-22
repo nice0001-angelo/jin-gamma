88b1b9ad16429471978b2b59cfcfb5bcd5a0cbfd
nice0001-angelo <nice0001@gmail.com>
2020-02-22 02:13:00 -0500

    update FreeboardComment model

--- src/main/java/net/jin/repository/FreeboardCommentRepository.java
+++ src/main/java/net/jin/repository/FreeboardCommentRepository.java
@@ -1,8 +1,11 @@
 package net.jin.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import net.jin.model.FreeboardComment;
+
 @Repository
-public interface FreeboardCommentRepository {
+public interface FreeboardCommentRepository extends JpaRepository<FreeboardComment, Long>{
 
 }
