23a7802703f0fef828d83811b1babff58de52b80
nice0001-angelo <nice0001@gmail.com>
2020-02-25 03:13:55 -0500

    update FreeboardCommentController

--- src/main/java/net/jin/controller/FreeboardCommentController.java
+++ src/main/java/net/jin/controller/FreeboardCommentController.java
@@ -7,17 +7,62 @@
  */
 package net.jin.controller;
 
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import net.jin.model.Freeboard;
+import net.jin.model.FreeboardComment;
+import net.jin.repository.FreeboardCommentRepository;
+import net.jin.service.freeboardComment.FreeboardCommentListService;
+
 
 @Controller
 public class FreeboardCommentController {
 	
+	@Autowired
+	private FreeboardComment freeboardComment;
+	
+	@Autowired
+	private FreeboardCommentRepository freeboardCommentRepository;
+	
+	@Autowired
+	private HttpSession httpSession;
+	
+	@Autowired
+	private FreeboardCommentListService freeboardCommentListService;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
 	@GetMapping(value = "/freeboardCommentList", produces = MediaTypes.HAL_JASON_UTF8_VALUE) 
 	public @ResponseBody String freeboardCommentList(){ //JASON 타입으로 변하게 할 수 있다
-		return null;
+		Freeboard freeboard = (Freeboard) httpSession.getAttribute("freeboard");
+		List<FreeboardComment> commentList = freeboardCommentListService.getList(freeboard.getFreeboardid());
+		String value = null;
+		try {
+			value = objectMapper.writeValueAsString(commentList);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return value;
+	}
+	
+	@PostMapping(value = "/freeboardCommentWrite")
+	public @ResponseBody String freeboardCommentWrite(@RequestBody FreeboardComment freeboardComment) {
+		freeboardComment.setWritedate(LocalDateTime.now());
+		freeboardCommentRepository.save(freeboardComment);
+		return "";
 	}
 }
