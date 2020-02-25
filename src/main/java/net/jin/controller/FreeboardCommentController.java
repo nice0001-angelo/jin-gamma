ef18d705a10b4e4919dd528493b8fadced86d149
nice0001-angelo <nice0001@gmail.com>
2020-02-24 21:26:21 -0500

    refactoring

--- src/main/java/net/jin/controller/FreeboardCommentController.java
+++ src/main/java/net/jin/controller/FreeboardCommentController.java
@@ -10,15 +10,14 @@ package net.jin.controller;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FreeboardCommentController {
 	
-	@RequestMapping("/freeboardCommentList")
-	@ResponseBody //Json 형태로 반환 하게 할 수 있다
-	public Map<String, String> freeboardCommentList(){
+	@GetMapping(value = "/freeboardCommentList", produces = MediaTypes.HAL_JASON_UTF8_VALUE) 
+	public @ResponseBody String freeboardCommentList(){ //JASON 타입으로 변하게 할 수 있다
 		return null;
 	}
 }
