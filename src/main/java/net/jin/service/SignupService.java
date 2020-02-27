3a837fd58209a294342f172b05c704b57e84b78c
nice0001-angelo <nice0001@gmail.com>
2020-02-27 02:10:38 -0500

    refactoring

--- src/main/java/net/jin/service/SignupService.java
+++ src/main/java/net/jin/service/SignupService.java
@@ -5,18 +5,33 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import net.jin.model.Users;
+import net.jin.repository.UsersRepository;
+
 @Service
 public class SignupService {
+
+	@Autowired
+	private UsersRepository usersRepository;
 	
 	public String signupUser(HttpServletRequest request) {
 		String userId = (String) request.getAttribute("user_id"); // return Object
-		String userPassword = (String) request.getAttribute("user_pw"); //return Object
-		String userName = request.getParameter("user_name"); //return String
+		String userPassword = (String) request.getAttribute("user_pw"); // return Object
+		String userName = request.getParameter("user_name"); // return String
 		
-		if(userId.isBlank()||userPassword.isBlank()||userName.isBlank()) {
-			return "signup";
+		  
+		if(userId==null||userPassword==null||userName==null) { 
+			  return "signup"; 
 		}
 		
+		Users users = new Users();
+		users.setUserid(userId);
+		users.setPassword(userPassword);
+		users.setUsername(userName);
+		
+		usersRepository.save(users);
+		 
+
 		return "index";
 	}
 
