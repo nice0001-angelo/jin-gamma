b4fae0c573651d742871c40efa6fdb24c3cc2235
nice0001-angelo <nice0001@gmail.com>
2020-02-27 04:10:38 -0500

    refactoring

--- src/main/java/net/jin/controller/UsersController.java
+++ src/main/java/net/jin/controller/UsersController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import net.jin.service.JoinService;
 import net.jin.service.LoginService;
 import net.jin.service.SignupService;
 
