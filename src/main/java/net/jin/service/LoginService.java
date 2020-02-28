d2a903ee35bc5503c6d7173a4d392b6680cbabbb
nice0001-angelo <nice0001@gmail.com>
2020-02-27 19:48:22 -0500

    refactoring

--- src/main/java/net/jin/service/LoginService.java
+++ src/main/java/net/jin/service/LoginService.java
@@ -10,6 +10,12 @@ public class LoginService {
 	public String loginUser(HttpServletRequest request) {
 		String userId = request.getParameter("user_id");
 		String userPassword = request.getParameter("user_pw");
+		
+		if (userId.equals("")||userPassword.equals("")) {
+			return "login";
+		}
+		
+		
 		return "";
 	}
 
