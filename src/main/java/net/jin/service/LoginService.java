5f099a77e1b963718dc760e46cff96b0b37c0949
nice0001-angelo <nice0001@gmail.com>
2020-02-27 19:44:49 -0500

    refactoring

--- src/main/java/net/jin/service/LoginService.java
+++ src/main/java/net/jin/service/LoginService.java
@@ -1,9 +1,16 @@
 package net.jin.service;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
 	
+	public String loginUser(HttpServletRequest request) {
+		String userId = request.getParameter("user_id");
+		String userPassword = request.getParameter("user_pw");
+		return "";
+	}
 
 }
