ed58e3f7d4ed35ac439d15e26d36cd191d4e42cc
nice0001-angelo <nice0001@gmail.com>
2020-02-27 20:38:48 -0500

    refactoring

--- src/main/java/net/jin/service/LoginService.java
+++ src/main/java/net/jin/service/LoginService.java
@@ -1,6 +1,11 @@
 package net.jin.service;
 
+import java.util.Enumeration;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +23,9 @@ public class LoginService {
 	@Autowired
 	private UsersRepository usersRepository;
 	
+	@Autowired
+	private HttpSession httpSession;
+		
 	public String loginUser(HttpServletRequest request) {
 		String userId = request.getParameter("user_id");
 		String userPassword = request.getParameter("user_pw");
@@ -29,10 +37,13 @@ public class LoginService {
 		String hashedPassword = userPasswordHashClass.getSHA256(userPassword);
 		Users users = usersRepository.findByUseridAndPassword(userId, hashedPassword);
 		
-		if(users.equals("")) {
+		if(users == null) {
 			return "login";
 		}
-		
+
+		httpSession.setAttribute("LoginedUser", users);
+		//httpSession.setAttribute("LoginUser", users);
+			
 		return "index";
 	}
 
