f0d5e5193cf1973f6c965c1c95e273c3e67f42e4
nice0001-angelo <nice0001@gmail.com>
2020-02-28 22:40:10 -0500

    add Model for google login

---
+++ src/main/java/net/jin/model/user/Role.java
@@ -0,0 +1,5 @@
+package net.jin.model.user;
+
+public class Role {
+
+}
--- src/main/java/net/jin/model/user/user.java
+++ src/main/java/net/jin/model/user/user.java
@@ -7,6 +7,54 @@
  */
 package net.jin.model.user;
 
-public class user {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column
+	private String picture;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Role role;
+	
+	public User(String name, String email, String picture, Role role) {
+		this.name = name;
+		this.email = email;
+		this.picture = picture;
+		this.role = role;
+	}
+	
+	public User update(String name, String picture) {
+		this.name = name;
+		this.picture = picture;
+		
+		return this;
+	}
+	
+	public String getRolekey() {
+		return this.role.getkey();
+	}
 }
