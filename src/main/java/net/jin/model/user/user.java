c6b8c241ba7c931f3b39763522ba8a5662cfb2ea
nice0001-angelo <nice0001@gmail.com>
2020-02-28 22:14:35 -0500

    add for google login

---
+++ src/main/java/net/jin/model/user/user.java
@@ -0,0 +1,12 @@
+/*
+ * This is a Jin-gamma Project
+ * File name : User.java
+ * Created by : Jinhyun
+ * Created on : Jan 2020
+ * Contents : User Model Object for google login
+ */
+package net.jin.model.user;
+
+public class user {
+
+}
