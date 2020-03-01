71b14c8d88b30f38618f8a255a023d5f75b80332
nice0001-angelo <nice0001@gmail.com>
2020-03-01 01:22:49 -0500

    update JUNIT for RestController Check

--- src/main/java/net/jin/controller/HelloController.java
+++ src/main/java/net/jin/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 	
 	@GetMapping("/hello")
 	public String hello() {
-		return "hello";
+		return "hello RestController for API Check";
 	}
 
 }
--- src/test/java/net/jin/controller/HelloControllerTest.java
+++ src/test/java/net/jin/controller/HelloControllerTest.java
@@ -2,13 +2,13 @@ package net.jin.controller;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -16,18 +16,19 @@ import org.springframework.test.web.servlet.ResultMatcher;
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class HelloControllerTest {
 	
 	@Autowired
-	private MockMvc mvc;
+	private MockMvc mockMvc;
 	
 	@Test
 	public void helloTest() throws Exception{
 		
-		String hello = "hello";
+		String hello = "hello RestController for API Check";
 		
-		mvc.perform(get("/hello"))
+		mockMvc.perform(get("/hello"))
 		.andExpect(status().isOk())
 		.andExpect((ResultMatcher)content().string(hello));
 
