b50439c8ae2053a927002e15fb3b4b440732b74a
nice0001-angelo <nice0001@gmail.com>
2020-03-01 00:45:34 -0500

    update for JUNIT test

---
+++ src/test/java/net/jin/controller/HelloControllerTest.java
@@ -0,0 +1,35 @@
+package net.jin.controller;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.annotation.security.RunAs;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@Test
+	public void hello_return() throws Exception{
+		String hello = "hello";
+		
+		mvc.perform(get("/hello"))
+		.andExpect(status().isOk())
+		.andExpect(content().string(hello));
+	}
+}
