1750ecaa4257fa7ebe4bde0622fd58b82f759812
nice0001-angelo <nice0001@gmail.com>
2020-03-01 00:58:12 -0500

    update for JUNIT

--- src/test/java/net/jin/controller/HelloControllerTest.java
+++ src/test/java/net/jin/controller/HelloControllerTest.java
@@ -2,9 +2,8 @@ package net.jin.controller;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import javax.annotation.security.RunAs;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 
 
@@ -26,10 +24,12 @@ public class HelloControllerTest {
 	
 	@Test
 	public void hello_return() throws Exception{
+		
 		String hello = "hello";
 		
 		mvc.perform(get("/hello"))
 		.andExpect(status().isOk())
-		.andExpect(content().string(hello));
+		.andExpect((ResultMatcher)content().string(hello));
+
 	}
 }
