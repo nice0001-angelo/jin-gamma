a5d490b4b8afad447ad8bf381aa1171748e8bb4b
nice0001-angelo <nice0001@gmail.com>
2020-03-02 00:51:11 -0500

    Junit test

--- src/test/java/net/jin/controller/HelloControllerTest.java
+++ src/test/java/net/jin/controller/HelloControllerTest.java
@@ -2,6 +2,7 @@ package net.jin.controller;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -16,7 +18,7 @@ import org.springframework.test.web.servlet.ResultMatcher;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 public class HelloControllerTest {
 	
@@ -30,7 +32,8 @@ public class HelloControllerTest {
 		
 		mockMvc.perform(get("/hello"))
 		.andExpect(status().isOk())
-		.andExpect((ResultMatcher)content().string(hello));
+		.andExpect((ResultMatcher)content().string(hello))
+		.andDo(print());
 
 	}
 }
