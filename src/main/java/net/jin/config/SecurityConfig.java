321c1f2061f91e4e2e525af063cfd1d8ecc61048
nice0001-angelo <nice0001@gmail.com>
2020-03-03 02:55:24 -0500

    add SecurityConfig.java

---
+++ src/main/java/net/jin/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package net.jin.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+}
