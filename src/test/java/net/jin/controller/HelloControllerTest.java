34caa3cdc8afb348113eb0d40dd88178db7f860a
nice0001-angelo <nice0001@gmail.com>
2020-03-03 00:28:36 -0500

    update JUNIT

--- src/test/java/net/jin/controller/HelloControllerTest.java
+++ src/test/java/net/jin/controller/HelloControllerTest.java
@@ -15,8 +15,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
-
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
