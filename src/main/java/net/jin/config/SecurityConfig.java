056d70cfa76ab1c8f563e6757e313967d80ec6c4
nice0001-angelo <nice0001@gmail.com>
2020-03-04 02:27:34 -0500

    uypdate security

--- src/main/java/net/jin/config/SecurityConfig.java
+++ src/main/java/net/jin/config/SecurityConfig.java
@@ -1,15 +1,17 @@
-
-package net.jin.config;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.authorizeRequests().antMatchers("/").permitAll();
-	}
-}
+/*
+ * package net.jin.config;
+ * 
+ * import
+ * org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ * import org.springframework.security.config.annotation.web.configuration.
+ * EnableWebSecurity; import
+ * org.springframework.security.config.annotation.web.configuration.
+ * WebSecurityConfigurerAdapter;
+ * 
+ * @EnableWebSecurity public class SecurityConfig extends
+ * WebSecurityConfigurerAdapter {
+ * 
+ * @Override protected void configure(HttpSecurity httpSecurity) throws
+ * Exception { httpSecurity.authorizeRequests().antMatchers("/").permitAll(); }
+ * }
+ */
