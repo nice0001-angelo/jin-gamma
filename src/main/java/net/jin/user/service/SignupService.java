a519c93677a5ce94136773fc426044ee37617561
nice0001-angelo <nice0001@gmail.com>
2020-04-27 03:38:46 -0400

    refactoring

--- src/main/java/net/jin/user/service/SignupService.java
+++ src/main/java/net/jin/user/service/SignupService.java
@@ -8,34 +8,7 @@ import org.springframework.stereotype.Service;
 import net.jin.model.Users;
 import net.jin.repository.UsersRepository;
 
-@Service
-public class SignupService {
 
-	@Autowired
-	private UsersRepository usersRepository;
-	
-	@Autowired
-	private UserPasswordHashClass userPasswordHashClass;
-	
-	public String signupUser(HttpServletRequest request) {
-		String userId = request.getParameter("user_id"); 
-		String userPassword = request.getParameter("user_pw"); 
-		String userName = request.getParameter("user_name"); 
-		
-		  
-		if(userId.equals("")||userPassword.equals("")||userName.equals("")) { 
-			  return "signup"; 
-		}
-		
-		Users users = new Users();
-		String hashedPassword = userPasswordHashClass.getSHA256(userPassword);
-		
-		users.setUserid(userId);
-		users.setPassword(hashedPassword);
-		users.setUsername(userName);
-		
-		usersRepository.save(users);
-		return "index";
-	}
-
+public interface SignupService {
+	public String signupUser(HttpServletRequest request);
 }
---
+++ src/main/java/net/jin/user/service/impl/SignupServiceImpl.java
@@ -0,0 +1,44 @@
+package net.jin.user.service.impl;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.jin.model.Users;
+import net.jin.repository.UsersRepository;
+import net.jin.user.service.SignupService;
+import net.jin.user.service.UserPasswordHashClass;
+
+@Service
+public class SignupServiceImpl implements SignupService {
+
+	@Autowired
+	private UsersRepository usersRepository;
+	
+	@Autowired
+	private UserPasswordHashClass userPasswordHashClass;
+	
+	@Override
+	public String signupUser(HttpServletRequest request) {
+		String userId = request.getParameter("user_id"); 
+		String userPassword = request.getParameter("user_pw"); 
+		String userName = request.getParameter("user_name"); 
+		
+		  
+		if(userId.equals("")||userPassword.equals("")||userName.equals("")) { 
+			  return "signup"; 
+		}
+		
+		Users users = new Users();
+		String hashedPassword = userPasswordHashClass.getSHA256(userPassword);
+		
+		users.setUserid(userId);
+		users.setPassword(hashedPassword);
+		users.setUsername(userName);
+		
+		usersRepository.save(users);
+		return "index";
+	}
+
+}
