fa84e4e248599f9f23c37c4e756cc7725180d741
nice0001-angelo <nice0001@gmail.com>
2020-04-28 01:25:36 -0400

    refactoring

--- src/main/java/net/jin/freeboard/service/FreeboardListService.java
+++ src/main/java/net/jin/freeboard/service/FreeboardListService.java
@@ -7,49 +7,6 @@
  */
 package net.jin.freeboard.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-
-import net.jin.model.Freeboard;
-import net.jin.pageMaker.PageMaker;
-import net.jin.repository.FreeboardRepository;
-
-@Service
-public class FreeboardListService {
-	
-	@Autowired
-	private FreeboardRepository freeboardRepository;
-	
-	@Autowired
-	private HttpSession session;
-	
-	@Autowired
-	private PageMakerService pageMakerService;
-
-	public String freeboardList(int pageNum){
-		
-		PageMaker pageMaker = pageMakerService.generatePageMaker(pageNum, 5, freeboardRepository);
-		
-		PageRequest pageRequest = PageRequest.of(pageNum-1, 5, Sort.Direction.DESC , "freeboardid");
-		Page<Freeboard> freeboardPage = freeboardRepository.findAll(pageRequest);
-		
-		if(freeboardPage.getSize() == 0) {
-			session.setAttribute("boardList", new ArrayList<Freeboard>());
-			session.setAttribute("pageMaker", pageMaker);
-			return "freeboardMain";
-		}
-		List<Freeboard> freeboardList = freeboardPage.getContent();
-		session.setAttribute("boardList", freeboardList);
-		session.setAttribute("pageMaker", pageMaker);
-		return "freeboardMain";
-	}
-
+public interface FreeboardListService {
+	public String freeboardList(int pageNum);
 }
