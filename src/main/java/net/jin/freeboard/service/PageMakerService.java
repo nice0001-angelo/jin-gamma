e13d25a18f662e0a1ae7bb6a93d5812081a37f57
nice0001-angelo <nice0001@gmail.com>
2020-04-28 01:32:06 -0400

    refactoring

--- src/main/java/net/jin/freeboard/service/PageMakerService.java
+++ src/main/java/net/jin/freeboard/service/PageMakerService.java
@@ -13,22 +13,6 @@ import org.springframework.stereotype.Service;
 import net.jin.model.Freeboard;
 import net.jin.pageMaker.PageMaker;
 
-@Service
-public class PageMakerService {
-	public PageMaker generatePageMaker(int pageNum, int contentNum, JpaRepository<Freeboard, Long> repository) {
-		PageMaker pageMaker = new PageMaker();
-		
-		int totalCount = (int)repository.count();
-		pageMaker.setTotalcount(totalCount);//전체 게시글 수를 지정한다
-		pageMaker.setPagenum(pageNum-1);//현재 페이지를 페이지 객체에 지정한다. -1을 해야 쿼리에서 사용할 수 있다
-		pageMaker.setContentnum(contentNum);//한페이지에서 몇개씩 게시글을 보여줄지 지정한다
-		pageMaker.setCurrentblock(pageNum);//현재페이지 블록이 몇번인지 현재 페이지 번호를 통해서 지정한다.
-		pageMaker.setLastblock(pageMaker.getTotalcount());//마지막 블록 번호를 전체 게시글 수를 통해서 정한다
-		pageMaker.prevNext(pageNum);//현재 페이지 번호로 화살표를 나타낼지 정한다
-		pageMaker.setStartPage(pageMaker.getCurrentblock());//시작페이지를 페이지 블록 번호로 정한다
-		pageMaker.setEndPage(pageMaker.getLastblock(),pageMaker.getCurrentblock());//마지막페이지를 마지막페이지블록과 현재 페이지블록 번호로 정한다
-		
-		return pageMaker;
-	}
-
+public interface PageMakerService {
+	public PageMaker generatePageMaker(int pageNum, int contentNum, JpaRepository<Freeboard, Long> repository);
 }
